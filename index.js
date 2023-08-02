1bafd714a60969e4f4566c8714e7a522bc9f23e6
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-08-02 13:51:06 +0700

    1

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
 import VisitorRoutes from "./routes/VisitorRoutes.js";
+// import http from "http";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/visitor', VisitorRoutes);
-app.listen(5000, () => console.log('Server running at port 5000'));
+app.listen(443, () => console.log('Server running at port 443'));
 
+// http.createServer(function(req, res) {
+//   res.write("Bot Alive!");
+//   res.end();
+// }).listen(8080)
